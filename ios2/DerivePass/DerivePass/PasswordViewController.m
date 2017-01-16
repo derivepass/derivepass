7819277b205f8be575bb35191ff664fd1c54a063
Fedor Indutny <fedor@indutny.com>
2017-01-16 09:34:45 -0500

    ios2: password confirmation on double-tap

--- ios2/DerivePass/DerivePass/PasswordViewController.m
+++ ios2/DerivePass/DerivePass/PasswordViewController.m
@@ -12,20 +12,28 @@
 #import "ApplicationsTableViewController.h"
 
 #import <CommonCrypto/CommonDigest.h>
+#import <QuartzCore/QuartzCore.h>
 
 #include <stdint.h>
 #include <string.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
+static NSString* const kDefaultEmoji = @"😬";
+static NSString* const kMasterPlaceholder = @"Master Password";
+static NSString* const kConfirmPlaceholder = @"Confirm Password";
+
 @interface PasswordViewController ()
 
 @property(weak, nonatomic) IBOutlet UITextField* masterPassword;
 @property(weak, nonatomic) IBOutlet UILabel* emojiLabel;
+@property(weak, nonatomic) IBOutlet UILabel* emojiConfirmationLabel;
 
 @end
 
-@implementation PasswordViewController
+@implementation PasswordViewController {
+  NSString* confirming_;
+}
 
 
 - (void)viewDidLoad {
@@ -35,8 +43,13 @@
 
 
 - (void)viewWillAppear:(BOOL)animated {
-  self.emojiLabel.text = @"😬";
+  confirming_ = nil;
+  self.emojiLabel.text = kDefaultEmoji;
+  self.emojiConfirmationLabel.text = kDefaultEmoji;
+  self.emojiConfirmationLabel.alpha = 0.0;
   self.masterPassword.text = @"";
+  self.masterPassword.placeholder = kMasterPlaceholder;
+  self.masterPassword.returnKeyType = UIReturnKeyDone;
   [self.navigationController setNavigationBarHidden:YES];
 }
 
@@ -104,11 +117,14 @@
       "🔍",      "🔒",      "🍴"};
 
   NSString* value = [((UITextField*)sender)text];
-  const char* utf8value = [value UTF8String];
+  const char* utf8value = value.UTF8String;
 
   // No password - display default emoji
-  if ([value length] == 0) {
-    self.emojiLabel.text = @"😬";
+  if (value.length == 0) {
+    if (confirming_)
+      self.emojiConfirmationLabel.text = kDefaultEmoji;
+    else
+      self.emojiLabel.text = kDefaultEmoji;
     return;
   }
 
@@ -139,12 +155,93 @@
     p += n;
   }
 
-  self.emojiLabel.text = [NSString stringWithUTF8String:emoji_fingerprint];
+  NSString* res = [NSString stringWithUTF8String:emoji_fingerprint];
+  if (confirming_)
+    self.emojiConfirmationLabel.text = res;
+  else
+    self.emojiLabel.text = res;
 }
 
 
 - (IBAction)onSubmitPassword:(id)sender {
+  if (confirming_) {
+    if (self.masterPassword.text != confirming_) {
+      UITextField* f = self.masterPassword;
+
+      CABasicAnimation* a = [CABasicAnimation animationWithKeyPath:@"position"];
+      [a setDuration:0.1];
+      [a setRepeatCount:2];
+      [a setAutoreverses:YES];
+      [a setFromValue:[NSValue valueWithCGPoint:CGPointMake(f.center.x - 5,
+                                                            f.center.y)]];
+      [a setToValue:[NSValue valueWithCGPoint:CGPointMake(f.center.x + 5,
+                                                          f.center.y)]];
+      [f.layer addAnimation:a forKey:@"position"];
+      return;
+    }
+    
+    self.view.userInteractionEnabled = NO;
+    [self hideConfirmation:^() {
+      self.view.userInteractionEnabled = YES;
+      [self performSegueWithIdentifier:@"ToApplications" sender:self];
+    }];
+    return;
+  }
+  
   [self performSegueWithIdentifier:@"ToApplications" sender:self];
+}
+
+
+- (void) hideConfirmation: (void (^)()) completion {
+  if (confirming_ == nil)
+    return completion();
+  
+  UILabel* original = self.emojiLabel;
+  UILabel* conf = self.emojiConfirmationLabel;
+  
+  self.masterPassword.text = confirming_;
+  self.masterPassword.placeholder = kMasterPlaceholder;
+  self.masterPassword.returnKeyType = UIReturnKeyDone;
+  confirming_ = nil;
+  
+  [UIView animateWithDuration:0.3
+                   animations:^{
+                     conf.center = original.center;
+                     conf.alpha = 0.0;
+                   } completion:^(BOOL finished) {
+                     completion();
+                   }];
+}
+
+
+- (IBAction)onEmojiTap:(id)sender {
+  if (confirming_) {
+    [self hideConfirmation: nil];
+    return;
+  }
+  
+  UILabel* original = self.emojiLabel;
+  UILabel* conf = self.emojiConfirmationLabel;
+  
+
+  // No transition when master password is empty
+  if (self.masterPassword.text.length == 0) return;
+
+  conf.center = original.center;
+  confirming_ = self.masterPassword.text;
+
+  self.masterPassword.text = @"";
+  self.masterPassword.placeholder = kConfirmPlaceholder;
+  self.masterPassword.returnKeyType = UIReturnKeyNext;
+
+  conf.text = kDefaultEmoji;
+  [UIView animateWithDuration:0.3
+                   animations:^() {
+                     conf.center = CGPointMake(
+                         conf.center.x,
+                         conf.center.y + conf.frame.size.height * 1.2);
+                     conf.alpha = 1.0;
+                   }];
 }
 
 @end

2cc96ab5c6f0c414661ce43a915f801338fcb630
Fedor Indutny <fedor@indutny.com>
2017-01-21 09:56:31 -0500

    ios2: remove boilerplate from test

--- ios2/DerivePass/DerivePassUITests/DerivePassUITests.m
+++ ios2/DerivePass/DerivePassUITests/DerivePassUITests.m
@@ -21,28 +21,15 @@ static const int kApplicationCount = 3;
 
 - (void)setUp {
   [super setUp];
-
-  // Put setup code here. This method is called before the invocation of each
-  // test method in the class.
-
-  // In UI tests it is usually best to stop immediately when a failure occurs.
   self.continueAfterFailure = NO;
-  // UI tests must launch the application that they test. Doing this in setup
-  // will make sure it happens for each test method.
   [[[XCUIApplication alloc] init] launch];
-
-  // In UI tests it’s important to set the initial state - such as interface
-  // orientation - required for your tests before they run. The setUp method is
-  // a good place to do this.
 }
 
 - (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each
-  // test method in the class.
   [super tearDown];
 }
 
-- (void)testExample {
+- (void)testMainFlow {
   uint64_t r;
   int err = SecRandomCopyBytes(kSecRandomDefault, 8, (void*)&r);
   XCTAssertEqual(err, 0);
